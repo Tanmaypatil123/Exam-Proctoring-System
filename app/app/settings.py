2ced209beadfdea0b86a1e151f8cb4d7653c975a
Tanmaypatil123 <tanmaypatil3151@gmail.com>
2023-11-07 15:18:27 +0530

    Added exam basic logic template

--- app/app/settings.py
+++ app/app/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
 from pathlib import Path
+from datetime import timedelta
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -37,7 +38,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'rest_framework',
+    "corsheaders",
+    "rest_framework",
+    "rest_framework_simplejwt",
     'users',
     'exam',
     'management',
@@ -46,6 +49,7 @@ INSTALLED_APPS = [
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
@@ -74,6 +78,13 @@ TEMPLATES = [
 WSGI_APPLICATION = 'app.wsgi.application'
 
 AUTH_USER_MODEL = 'users.UserModel'
+
+REST_FRAMEWORK = {
+    'DEFAULT_AUTHENTICATION_CLASSES': (
+        'rest_framework_simplejwt.authentication.JWTAuthentication',
+    )
+}
+
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
@@ -128,3 +139,28 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 import os
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+
+SIMPLE_JWT = {
+    'ACCESS_TOKEN_LIFETIME': timedelta(minutes=(60*10)),
+    'REFRESH_TOKEN_LIFETIME': timedelta(days=5),
+
+    'AUTH_HEADER_TYPES': ('Bearer',),
+    'AUTH_HEADER_NAME': 'HTTP_AUTHORIZATION',
+    'USER_ID_FIELD': 'id',
+    'USER_ID_CLAIM': 'user_id',
+    'USER_AUTHENTICATION_RULE': 'rest_framework_simplejwt.authentication.default_user_authentication_rule',
+
+    'AUTH_TOKEN_CLASSES': ('rest_framework_simplejwt.tokens.AccessToken',),
+    'TOKEN_TYPE_CLAIM': 'token_type',
+    'TOKEN_USER_CLASS': 'rest_framework_simplejwt.models.TokenUser',
+
+    'JTI_CLAIM': 'jti',
+
+}
+
+PASSWORD_RESET_TIMEOUT=900          # 900 Sec = 15 Min
+
+CORS_ALLOWED_ORIGINS = [
+    "http://localhost:3000",
+    "http://127.0.0.1:3000",
+]
---
+++ app/exam/serializers.py
@@ -0,0 +1,46 @@
+from rest_framework import serializers
+from exam.models import *
+
+
+class ExamCreationSerializers(serializers.ModelSerializer):
+    class Meta :
+        model = Exam
+        fields = [
+            "name","description","no_of_questions","max_warning_limit"
+        ]
+
+class QuestionCreation(serializers.Serializer):
+    exam_id = serializers.CharField()
+    question = serializers.CharField()
+    
+    class Meta :
+        
+        fields = [
+            "question","exam_id"
+        ]
+
+class OptionCreationSerializer(serializers.Serializer):
+    question_id = serializers.CharField()
+    option = serializers.CharField()
+    
+    class Meta :
+        
+        fields = [
+            "option","question_id"
+        ]
+
+class ResponseCreationSerializer(serializers.Serializer):
+    response = models.CharField()
+    
+    class Meta :
+        fields = [
+            "option_id"
+        ]
+
+class WarningCreationSerializers(serializers.Serializer):
+    message = models.CharField()
+
+    class Meta:
+        fields = [
+            "message"
+        ]
