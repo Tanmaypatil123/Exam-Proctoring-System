a08320550ba180276e3ad9b470811a36ad3b7440
Tanmaypatil123 <tanmaypatil3151@gmail.com>
2023-11-16 23:14:03 +0530

    added cam worker for upper body detection

--- client/pages/admin_dashboard_login_page.py
+++ client/pages/admin_dashboard_login_page.py
@@ -103,3 +103,12 @@ class Admin_Dashboard_window(QtWidgets.QMainWindow,admin_dashboard_page):
     def __init__(self, parent=None):
         super().__init__(parent)
         self.setupUi(self)
+
+
+if __name__ == "__main__":
+    import sys
+    app = QtWidgets.QApplication(sys.argv)
+    Dialog = QtWidgets.QDialog()
+    ui = Admin_Dashboard_window()
+    ui.show()
+    sys.exit(app.exec_())
