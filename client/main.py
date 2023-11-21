5d7c2de8eacc299c8caf8b35d07aa5d090e6c0da
Tanmaypatil123 <tanmaypatil3151@gmail.com>
2023-11-22 00:02:09 +0530

    removed some uneccesary

--- client/main.py
+++ client/main.py
@@ -6,8 +6,9 @@ from PyQt5.QtWidgets import QWidget
 from pages.candidate_login_page_for_exam import Candidate_login_Window
 from pages.Candidate_System_check_and_block import SystemCheckWindow
 from pages.instruction_page import InstructionWindow
-from pages.question_atempting_screen_candidate_view import Question_attempting_window
-from pages.question import QuestionWindow
+# from pages.question_atempting_screen_candidate_view import Question_attempting_window
+# from pages.question import QuestionWindow
+from pages.question_new import Question_attempting_window
 
 
 def load_questions():
@@ -35,7 +36,8 @@ class Window(QtWidgets.QMainWindow):
         # following staked widgetstores pages or gui class related to software
         self.stacked_widget = QtWidgets.QStackedWidget()
         self.stacked_widget.setFixedSize(self.width,self.height)
-
+        self.question_data = load_questions()
+        self.current_question = 1
         self.setCentralWidget(self.stacked_widget)
         self.setGeometry(0,0,self.width,self.height)
 
@@ -45,6 +47,8 @@ class Window(QtWidgets.QMainWindow):
         ## Candidate login page index - 0
         self.candidate_login_window = Candidate_login_Window() 
         self.stacked_widget.addWidget(self.candidate_login_window)
+        # self.candidate_login_window.pushButton.clicked.connect(self.go_to_instruction_page)
+        self.candidate_login_window.pushButton.clicked.connect(self.go_to_question_attempting_page)
 
         ## System check page - 1
         self.systemcheck_window = SystemCheckWindow()
@@ -57,8 +61,36 @@ class Window(QtWidgets.QMainWindow):
         ## Question attempting window - 3
         self.attempting_window = Question_attempting_window()
         self.stacked_widget.addWidget(self.attempting_window)
+
+
+        self.attempting_window.set_question(
+            1,"hey" , ["h",'j','k','l']
+        )
+        self.connect_submit_button()
+
         ## go to first page
         self.go_to_candidate_login_page()
+
+    def go_to_instruction_page(self):
+        self.stacked_widget.setCurrentIndex(2)
+    
+    def go_to_question_attempting_page(self):
+        self.stacked_widget.setCurrentIndex(3)
+    def connect_submit_button(self):
+        self.attempting_window.pushButton_4.clicked.connect(self.load_next_question)
+    
+    def load_next_question(self):
+        # Increment to load the next question
+        self.current_question += 1
+        if self.current_question in self.question_data:
+            question_info = self.question_data[self.current_question]
+            question = question_info["question"]
+            options = question_info["options"]
+
+            self.attempting_window.set_question(self.current_question, question, options)
+        else:
+            # No more questions, handle completion or action
+            pass
 
     def go_to_candidate_login_page(self):
         self.stacked_widget.setCurrentIndex(0)
